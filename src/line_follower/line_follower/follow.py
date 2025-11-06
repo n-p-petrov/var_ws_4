639377a2d6c0091a00c448207a99a1d41ab61f3b
root <nikola.petrov@student.uva.nl>
2025-11-07 03:01:26 +0800

    spin up camera subscriber in line follow

--- src/line_follower/line_follower/follow.py
+++ src/line_follower/line_follower/follow.py
@@ -15,8 +15,8 @@ def compute_lines(rgb_image):
     return [(1, math.pi / 3), (2, 0.0)]
 
 def follow_line(drive_publisher, rgb_image):
-    hough_lines = hough_lines(rgb_image)
-    closest_line = min(hough_lines, key=0)
+    hough_lines = compute_lines(rgb_image)
+    closest_line = min(hough_lines, key=lambda x: x[0])
 
     drive_publisher.turn(compute_turn_angle(closest_line[1]), 0.25)
     drive_publisher.move_forward(2, math.pi)
@@ -24,7 +24,13 @@ def follow_line(drive_publisher, rgb_image):
 def main(args=None):
     rclpy.init(args=args)
 
+    print("Initializing the drive publisher...")
     drive_publisher = DrivePublisher()
+
+    print("Initializing the image subscriber...") 
     camera_subscriber = ImageSubscriber(partial(follow_line, drive_publisher))
+    rclpy.spin(camera_subscriber)
+
+    print("Line following finished")
 
     rclpy.shutdown()

ee3fbcd98f0d99a82c3c74d249bc0244958a24f5
n-p-petrov <nikola.petrov@student.uva.nl>
2025-11-07 11:21:52 +0100

    clean up line-following pipeline and add comments

--- src/line_follower/line_follower/follow.py
+++ src/line_follower/line_follower/follow.py
@@ -2,24 +2,42 @@ import math
 from functools import partial
 
 import rclpy
+from driver.driver_publisher import DrivePublisher
 from image_subscriber.image_subscriber import ImageSubscriber
 
-from driver.driver_publisher import DrivePublisher
+LINEAR_VELOCITY = 2 * math.pi  # radians per second (2pi = 1 wheel rotation per second)
+DURATION_LINEAR_MOVE = 3  # seconds
 
+ANGULAR_VELOCITY = math.pi / 4  # radians per second
 
-def compute_turn_angle(theta):
-    return math.pi / 2 - theta
 
 def compute_lines(rgb_image):
     # TODO: add the actual implementation here lol
     return [(1, math.pi / 3), (2, 0.0)]
 
+
 def follow_line(drive_publisher, rgb_image):
+    """
+    This function is executed for each image captured from the camera.
+
+    The idea is to perform a small corrections to the direction of the robot
+    on every frame of the ceiling such that it doesn't deviate too much from
+    the line it is following.
+
+    After each correction we move the robot forward for a bit and then repeat
+    with a new image.
+    """
     hough_lines = compute_lines(rgb_image)
     closest_line = min(hough_lines, key=lambda x: x[0])
 
-    drive_publisher.turn(compute_turn_angle(closest_line[1]), 0.25)
-    drive_publisher.move_forward(2, math.pi)
+    # face direction of line
+    rho, theta = closest_line[0], closest_line[1]
+    angle_to_parrallel = math.pi / 2 - thetha
+    drive_publisher.turn(angle_to_parrallel, ANGULAR_VELOCITY)
+
+    # move along line
+    drive_publisher.move_forward(DURATION_LINEAR_MOVE, LINEAR_VELOCITY)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -27,9 +45,9 @@ def main(args=None):
     print("Initializing the drive publisher...")
     drive_publisher = DrivePublisher()
 
-    print("Initializing the image subscriber...") 
-    camera_subscriber = ImageSubscriber(partial(follow_line, drive_publisher))
-    rclpy.spin(camera_subscriber)
+    print("Initializing the image subscriber...")
+    image_subscriber = ImageSubscriber(partial(follow_line, drive_publisher))
+    rclpy.spin(camera_image)
 
     print("Line following finished")
 

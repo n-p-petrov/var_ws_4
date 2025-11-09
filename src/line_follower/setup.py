f299827ea259c1746b82d6c2879b7826986aeb82
Weronika Orska <weronika.orska@icloud.com>
2025-11-09 19:28:46 +0100

    lines_processor

--- src/line_follower/setup.py
+++ src/line_follower/setup.py
@@ -25,7 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'follow = line_follower.follow:main',
-            'lines_pr = line_follower.lines_processor:main',
+            'lines_processor = line_follower.lines_processor:main',
         ],
     },
 )

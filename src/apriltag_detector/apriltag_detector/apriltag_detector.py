a0ac08c0f68464cab4d282dddf8c7cc420ce429e
root <nikola.petrov@student.uva.nl>
2025-11-13 22:59:27 +0800

    add partial fix for apriltag detection

--- src/apriltag_detector/apriltag_detector/apriltag_detector.py
+++ src/apriltag_detector/apriltag_detector/apriltag_detector.py
@@ -4,8 +4,8 @@ import cv2  # pip install opencv-python
 import numpy as np
 import rclpy
 from apriltag import apriltag  # pip install apriltag
-from apriltag_msgs.msg import AprilTagDetectionArray
-from geometry_msgs.msg import Point32, Polygon
+from apriltag_msgs.msg import AprilTagDetection, AprilTagDetectionArray
+from geometry_msgs.msg import Point, Polygon
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
 
@@ -20,27 +20,44 @@ class ApriltagDetector(Node):
         self.detections_publisher = self.create_publisher(
             AprilTagDetectionArray, "/apriltag_detections", 10
         )
+        self.family = "tagStandard41h12"
 
-        self.apriltagdetector = apriltag("tagStandard41h12")
+        self.apriltagdetector = apriltag(self.family)
 
     def listener_callback(self, image_buffer):
         np_arr = np.frombuffer(image_buffer.data, np.uint8)
         gray_image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
-        results = self.apriltagdetector.detect(gray_image)
+        detected_tags = self.apriltagdetector.detect(gray_image)
 
         detection_array = AprilTagDetectionArray()
+        detection_array.header.stamp = self.get_clock().now().to_msg()
+        detection_array.header.frame_id = "camera"
 
-        for r in results:
-            polygon = Polygon()
-            for x, y in r.corners:
-                polygon.points.append(Point32(x=float(x), y=float(y), z=0.0))
-            detection_array.detections.append(polygon)
+        for tag in detected_tags:
+            det = AprilTagDetection()
+            
+            det.family = self.family
+            det.id = int(tag['id'])
+            det.hamming = int(tag['hamming'])
+            det.decision_margin = float(tag['margin'])
+            det.goodness = float(tag['margin']) # how clearly the tag pattern stand out from the background
+
+            print(tag)
+            print(tag['center'])
+            det.centre = Point(
+                x = float(tag['center'][0])
+                y = float(tag['center'][1])
+                z = 0.0
+             )
+
+            orners = np.array(tag['lb-rb-rt-lt']).reshape(4, 2)
+            det.corners = [Point(x=float(x), y=float(y), z=0.0) for x, y in corners]
+
+            det.homography = [0.0]*9 # left empty for now can be added if needed
+
+            detection_array.detections.append(det)
 
         self.detections_publisher.publish(detection_array)
-        self.get_logger().info(
-            f"Published {len(detection_array.detections)} apriltag detections."
-        )
-
 
 def main(args=None):
     rclpy.init(args=args)

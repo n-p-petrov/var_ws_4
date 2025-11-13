7825ba36e474efec0a84515414a8a799f61ed6de
n-p-petrov <nikola.petrov@student.uva.nl>
2025-11-13 13:16:35 +0100

    add apriltag_detector

---
+++ src/apriltag_detector/apriltag_detector/apriltag_detector.py
@@ -0,0 +1,52 @@
+import argparse
+
+import cv2  # pip install opencv-python
+import numpy as np
+import rclpy
+from apriltag import apriltag  # pip install apriltag
+from geometry_msgs.msg import Point32, Polygon
+from rclpy.node import Node
+from sensor_msgs.msg import CompressedImage
+
+from apriltag_detector.msg import ApriltagDetectionArray
+
+
+class ApriltagDetector(Node):
+    def __init__(self):
+        super().__init__("apriltag_detector")
+
+        self.image_subscriber = self.create_subscription(
+            CompressedImage, "/image_rect/compressed", self.listener_callback, 10
+        )
+        self.detections_publisher = self.create_publisher(
+            ApriltagDetectionArray, "/apriltag_detections", 10
+        )
+
+        options = apriltag.DetectorOptions(families="tagStandard41h12")
+        self.apriltagdetector = apriltag.Detector(options)
+
+    def listener_callback(self, image_buffer):
+        np_arr = np.frombuffer(image_buffer.data, np.uint8)
+        gray_image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
+        results = self.apriltagdetector.detect(gray_image)
+
+        detection_array = ApriltagDetectionArray()
+
+        for r in results:
+            polygon = Polygon()
+            for x, y in r.corners:
+                polygon.points.append(Point32(x=float(x), y=float(y), z=0.0))
+            detection_array.detections.append(polygon)
+
+        self.detections_publisher.publish(detection_array)
+        self.get_logger().info(
+            f"Published {len(detection_array.detections)} apriltag detections."
+        )
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ApriltagDetector()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()

06bf59c711235ed43f0b0ae252f7eee9f7b90823
n-p-petrov <nikola.petrov@student.uva.nl>
2025-12-04 13:05:11 +0100

    change image subscriber to extract images from rosbag

--- src/image_subscriber/image_subscriber/image_subscriber.py
+++ src/image_subscriber/image_subscriber/image_subscriber.py
@@ -1,36 +1,38 @@
-# Image subscriber directly from /image_raw/compressed topic
-
-from time import time
-
 import cv2
-import numpy as np
 import rclpy
 from cv_bridge import CvBridge
 from rclpy.node import Node
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
 
 
 class ImageSubscriber(Node):
-    def __init__(self, callback):
+    def __init__(self):
         """
         Args:
             callback: A function that takes in a RGB frame and performs an action.
         """
         super().__init__("image_subscriber")
         self.bridge = CvBridge()
-        self.callback = callback
         self.subscription = self.create_subscription(
-            CompressedImage, "/image_rect/compressed", self.listener_callback, 1
+            Image, "/oak/rgb/image_raw", self.listener_callback, 10
         )
+        self.bridge = CvBridge()
+        self.counter = 0
 
     def listener_callback(self, msg):
-        np_arr = np.frombuffer(msg.data, np.uint8)
-        cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        bgr_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
-        if cv_image is not None:
-            rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-            self.callback(rgb_image)
-            self.get_logger().info("Pan tilt camera feed callback successfully executed for a frame.")
-        else:
-            self.get_logger().info("Pan tilt camera feed failed due to unsuccessful cv2.imdecode.")
+        if bgr_image is not None:
+            cv2.imwrite(f"./mapping/frame_{self.counter}.jpg", bgr_image)
+            self.get_logger().info(f"Frame {self.counter} saved.")
+            self.counter += 1
 
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageSubscriber()
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
--- src/image_subscriber/setup.py
+++ src/image_subscriber/setup.py
@@ -1,29 +1,27 @@
 from setuptools import find_packages, setup
 
-package_name = 'image_subscriber'
+package_name = "image_subscriber"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='nikola.petrov@student.uva.nl',
-    description='TODO: Package description',
-    license='Apache-2.0',
+    maintainer="root",
+    maintainer_email="nikola.petrov@student.uva.nl",
+    description="TODO: Package description",
+    license="Apache-2.0",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": ["save_imgs = image_subscriber.image_subscriber:main"],
     },
 )
